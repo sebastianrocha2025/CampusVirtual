1bb58580620eaee5d42696512c2312251f387af5
sebastianrocha2025 <sebastianrocha1990@gmail.com>
2026-03-22 15:39:12 -0300

    actualizado allow

--- CampusVirtual/settings.py
+++ CampusVirtual/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fkq5bu+30)vjnm6zdza++xaaq&q*17#shl!lzs-_3pnww2lmwe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
