5272e4342666c014bb3cd45b729f12bcc93d623d
sebastianrocha2025 <sebastianrocha1990@gmail.com>
2026-03-22 16:22:41 -0300

    fix superuser render

--- build.sh
+++ build.sh
@@ -11,20 +11,6 @@ python manage.py collectstatic --noinput
 
 echo "Creando superusuario director..."
 
-python manage.py shell << END
-
-from django.contrib.auth.models import User
-
-username = "director"
-email = "director@campus.com"
-password = "admin123"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print("Superusuario director creado")
-else:
-    print("El superusuario ya existe")
-
-END
+python manage.py createsuperuser --noinput || true
 
 echo "Build finalizado"
